71c7cc4ce9940cbefbdbfc55d6c0d4e2b69aa347
Gabriel Diniz <gabrieldiniz@Gabriels-MacBook-Air.local>
2024-06-22 19:42:23 -0300

    Feat: Card Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as FormComponent } from './FormComponent'
 export { default as Header } from './Header'
 export * from './BannerImage'
+export * from './CardComponent'
 export * from './Logo'
 export * from './StyledButton'
 export * from './StyledInput'
